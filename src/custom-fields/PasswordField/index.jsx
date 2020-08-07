435ed3ee25ee65ee1547838bdc6fff21acd08567
Kibuto <nguyentrongnhanc8@gmail.com>
2020-08-07 20:06:22 +0700

    password input strength (done)

--- src/custom-fields/PasswordField/index.jsx
+++ src/custom-fields/PasswordField/index.jsx
@@ -1,7 +1,7 @@
 // libs
 import { Form, Input, Popover, Progress } from "antd";
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React from "react";
 import { CloseCircleOutlined, CheckCircleOutlined } from "@ant-design/icons";
 // others
 import "./style.scss";
@@ -35,7 +35,6 @@ function PasswordField(props) {
     hasFeedBack,
   } = props;
 
-  const [percent, setPercent] = useState(0);
   const { errors, touched } = form;
   const { name, value } = field;
 
@@ -46,13 +45,16 @@ function PasswordField(props) {
   const touchedError = hasError && touchedErr;
 
   const regexTest = (pattern, letters) => {
-    if (pattern.test(letters)) {
-      //percent < 75 && setPercent(percent + 25);
-      return <CheckCircleOutlined style={{ color: "green" }} />;
-    } else {
-      //percent > 0 && setPercent(percent - 25);
-      return <CloseCircleOutlined style={{ color: "red" }} />;
-    }
+    if (pattern.test(letters)) return 1;
+    return 0;
+  };
+
+  const renderCondition = (pattern, letters) => {
+    return regexTest(pattern, letters) ? (
+      <CheckCircleOutlined style={{ color: "green" }} />
+    ) : (
+      <CloseCircleOutlined style={{ color: "red" }} />
+    );
   };
 
   const handleStrengthPassword = (
@@ -60,25 +62,35 @@ function PasswordField(props) {
       <p>Your password must have:</p>
       <ul className="rules">
         <li>
-          {regexTest(/(?=[A-Z])/, value)}
+          {renderCondition(/(?=[A-Z])/, value)}
           At least a Uppercase letter
         </li>
-        <li>{regexTest(/[a-z0-9]{8,}/, value)}8 or more characters</li>
+        <li>{renderCondition(/[a-z0-9]{8,}/, value)}8 or more characters</li>
         <li>
-          {regexTest(/[a-z][0-9]/, value)}
+          {renderCondition(/[a-z][0-9]/, value)}
           Contain both letters and numbers
         </li>
         <li>
-          {regexTest(/^[a-zA-Z0-9]*\S$/, value)}
+          {renderCondition(/^[a-zA-Z0-9]*\S$/, value)}
           Do not contain whitespace
         </li>
       </ul>
       <p>
-        <Progress percent={0} size="small" showInfo={false} />
+        <Progress
+          percent={
+            (regexTest(/(?=[A-Z])/, value) +
+              regexTest(/[a-z0-9]{8,}/, value) +
+              regexTest(/[a-z][0-9]/, value) +
+              regexTest(/^[a-zA-Z0-9]*\S$/, value)) *
+            25
+          }
+          size="small"
+          showInfo={false}
+        />
       </p>
     </div>
   );
-  console.log(percent);
+
   return (
     <div className="block-input">
       <Form.Item
